45b4c3a26b173d9b954bf668e4346efa73ab56a2
David Romero <d.romero@sapalomera.cat>
2024-12-07 15:13:04 +0100

    not fixed yet oauth

--- oauth/oauth.php
+++ oauth/oauth.php
@@ -5,7 +5,7 @@ function OAuth(){
     $callbackURL = 'https://www.davidromero.cat/oauth/callback.php'; //URL del callback que despues configuraremos
 
     //URL que usamos para "hablar" con github.
-    $URL = 'https://github.com/login/oauth/authorize?client_id=' . client_id . '&redirect_ur=l' . $callbackURL . '&scope=user:email';
+    $URL = 'https://github.com/login/oauth/authorize?client_id=' . client_id . '&redirect_uri=' . $callbackURL . '&scope=user:email';
     header('Location:' . $URL);
 }
 ?>
