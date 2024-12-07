b1e7f90db85eae71e7125d9ab3827809eed1a5be
David Romero <d.romero@sapalomera.cat>
2024-12-07 14:40:57 +0100

    login github 0.2

--- oauth/callback.php
+++ oauth/callback.php
@@ -1,3 +1,10 @@
 <?php 
-echo "github callback";
+require_once '../oauth/oauth.php';
+
+if(isset($_GET['code'])){
+    $code = $_GET['code'];
+    var_dump($code);
+} else {
+    echo "Hola";
+}
 ?>
