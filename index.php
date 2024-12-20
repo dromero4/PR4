9de5f64011e88b9246f8e3d19f8e9f921f9afe1f
David Romero <d.romero@sapalomera.cat>
2024-12-20 13:10:15 +0100

    `Removed JavaScript file card-index.js and updated navbar styles and functionality in vista/navbar.view.php`

--- index.php
+++ index.php
@@ -191,10 +191,9 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/styles.css">
-    <script src="JavaScript/card-index.js"></script>
-    <script src="JavaScript/search-bar.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </head>
 <body>
     <form action="#" method="post">
--- vista/navbar.view.php
+++ vista/navbar.view.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </head>
 <body>
 <?php
@@ -18,35 +21,42 @@ $vistaDir = BASE_URL . 'vista';
 if(isset($_SESSION['usuari'])){
     $admin = verificarAdmin($connexio, $_SESSION['correu']);
 }
-
-
-
 ?>
 
-<nav>
-    <div class="left">
-        <?php if (isset($_SESSION['usuari'])): ?>
-            <a href="<?= $vistaDir ?>/insertar.php">Insertar article</a>
-            <a href="<?= $vistaDir ?>/../index.php">Consultar articles</a>
-            
-
-        <?php endif; ?>
-    </div>
-
-    <div class="right">
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark" id="navbar">
+    <div class="container-fluid">
+        <a class="navbar-brand ms-2" href='<?= $vistaDir?>/../index.php'>Brand Padel</a>
         <?php if(isset($_SESSION['usuari']) && !$admin): ?>
-            <a href="<?= $vistaDir ?>/profile.php"><button>Perfil</button></a>
-            <a href="<?= $vistaDir ?>/reiniciarPassword.php"><button>Canviar contrassenya</button></a>
-            <a href="<?= $vistaDir ?>/../controlador/logout.php"><button>Logout</button></a>
+            <li class="nav-link dropdown" id="drpdwn">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                    <img src="<?= BASE_PATH?>imagenes/icones/settings-svgrepo-com.svg">
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end bg-dark">
+                    <li><a class="dropdown-item text-white" href="<?= $vistaDir?>/profile.php">Perfil</a></li>
+                    <li><a class="dropdown-item text-white" href="<?= $vistaDir?>">Canviar contrasenya</a></li>
+                    <li><a class="dropdown-item text-white" href="<?= $vistaDir?>">Logout</a></li>
+                </ul>
+             </li>
         <?php elseif(isset($_SESSION['usuari']) && $admin): ?>
-            <a href="<?= $vistaDir ?>/users.php"><button>Usuaris</button></a>
-            <a href="<?= $vistaDir ?>/profile.php"><button>Perfil</button></a>
-            <a href="<?= $vistaDir ?>/reiniciarPassword.php"><button>Canviar contrassenya</button></a>
-            <a href="<?= $vistaDir ?>/../controlador/logout.php"><button>Logout</button></a>
+             <li class="nav-link dropdown" id="drpdwn">
+             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                    <img src="<?= BASE_PATH?>imagenes/icones/settings-svgrepo-com.svg">
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end bg-dark">
+                    <li><a class="dropdown-item text-white" href="#">Usuaris</a></li>
+                    <li><a class="dropdown-item text-white" href="<?= $vistaDir?>/profile.php">Perfil</a></li>
+                    <li><a class="dropdown-item text-white" href="#">Canviar contrasenya</a></li>
+                    <li><a class="dropdown-item text-white" href="#">Logout</a></li>
+                </ul>
+             </li>
         <?php else:?>
-            <a href="<?= $vistaDir ?>/../index.php"><button>Consultar</button></a>
-            <a href="<?= $vistaDir ?>/login.php"><button>Login</button></a>
-            <a href="<?= $vistaDir ?>/signup.php"><button>Signup</button></a>
+            <li class="nav-link dropdown" id="drpdwn">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Get ready with us</a>
+                <ul class="dropdown-menu dropdown-menu-end bg-dark">
+                    <li><a class="dropdown-item text-white" href="#">Login</a></li>
+                    <li><a class="dropdown-item text-white" href="#">Signup</a></li>
+                </ul>
+             </li>
         <?php endif; ?>
     </div>
 </nav>
