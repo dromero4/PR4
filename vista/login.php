fa59856f38f55a8847334f10dfc8649a5e920e3c
David Romero <d.romero@sapalomera.cat>
2025-02-03 19:07:58 +0100

    3

--- vista/login.php
+++ vista/login.php
@@ -23,11 +23,11 @@ include_once '../lib/claus_recaptcha/claus.php';
 <body>
     <!-- Inputs diversos per poder inserir les dades de l'usuari -->
     <form id="form-login" action="<?php echo htmlspecialchars(dirname($_SERVER['PHP_SELF']) . '/../controlador/controlador.php'); ?>" method="post">
-        <label for="usuari">
+        <label for="usuari" hidden> Usuari:
             <input type="text" id="usuari" name="usuari" placeholder="Usuari" value="<?php echo $cookie_user ?>"><br><br>
         </label>
 
-        <label for="contrassenya">
+        <label for="contrassenya" hidden> Contrassenya:
             <input type="password" id="contrassenya" name="contrassenya" placeholder="Contrassenya" value="<?php echo $cookie_pass ?>"><br><br>
         </label>
 
