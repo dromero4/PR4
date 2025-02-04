b2305254a0c2265316e1fb290106713406718764
David Romero <d.romero@sapalomera.cat>
2025-02-04 09:54:54 +0100

    ?

--- database/connexio-api.php
+++ database/connexio-api.php
@@ -10,8 +10,8 @@ $contrasenya = DB_VAR['DB_PASSWORD2'];
 
 //  fitxer per a la connexio a la base de dades
 try {
-    $connexio = new PDO("mysql:host=$direccio;dbname=$nomBBDD;charset=utf8", $usuaris, $contrasenya);
-    $connexio->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connexio1 = new PDO("mysql:host=$direccio;dbname=$nomBBDD;charset=utf8", $usuaris, $contrasenya);
+    $connexio1->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "No s'ha pogut connectar a la base de dades..." . $e->getMessage();
 }
