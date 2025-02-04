78460f4f3612cced3e3e1e6f65064ee8c22ea0bf
David Romero <d.romero@sapalomera.cat>
2025-02-04 09:56:12 +0100

    ??

--- database/connexio-api.php
+++ database/connexio-api.php
@@ -3,14 +3,14 @@
 <?php
 require_once 'env-api.php';
 
-$direccio = DB_VAR['DB_HOST2'];
-$nomBBDD = DB_VAR['DB_NAME2'];
-$usuaris = DB_VAR['DB_USER2'];
-$contrasenya = DB_VAR['DB_PASSWORD2'];
+$direccio2 = DB_VAR['DB_HOST2'];
+$nomBBDD2 = DB_VAR['DB_NAME2'];
+$usuaris2 = DB_VAR['DB_USER2'];
+$contrasenya2 = DB_VAR['DB_PASSWORD2'];
 
 //  fitxer per a la connexio a la base de dades
 try {
-    $connexio1 = new PDO("mysql:host=$direccio;dbname=$nomBBDD;charset=utf8", $usuaris, $contrasenya);
+    $connexio1 = new PDO("mysql:host=$direccio2;dbname=$nomBBDD2;charset=utf8", $usuaris2, $contrasenya2);
     $connexio1->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "No s'ha pogut connectar a la base de dades..." . $e->getMessage();
